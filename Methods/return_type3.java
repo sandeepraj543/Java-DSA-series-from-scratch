db5a22a2ed6f3963353dcb5332c5d0c724987b1d
sandeepraj543 <sandeepkb805124@gmail.com>
2026-07-23 23:25:14 +0530

    add some changes

--- Methods/return_type3.java
+++ Methods/return_type3.java
@@ -1,12 +1,18 @@
+
 public class return_type3 {
-    public static int max(int a, int b, int c){  // parameter
-        if (a>=b && a>=c) return a;
-        else if (b>=a && b>=c) return b;
-        else return c;
+
+    public static int max(int a, int b, int c) {  // parameter
+        if (a >= b && a >= c) {
+            return a; 
+        }else if (b >= a && b >= c) {
+            return b; 
+        }else {
+            return c;
+        }
     }
 
-    public static void main(String[] args){
-        System.out.println(max(4,2,9));
+    public static void main(String[] args) {
+        System.out.println(max(4, 2, 9));
+        System.out.println(max(12, 8, 17));
     }
-    
 }
---
+++ Methods/testing.java
@@ -0,0 +1,15 @@
+// kuch v practice krne ke lea ye file hai.
+
+public class testing {
+
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(100));
+        System.out.println(Math.floor(7.8));
+        System.out.println(Math.floor(-7.8));
+        System.out.println(Math.ceil(7.8));
+        System.out.println(Math.ceil(-7.8));
+        System.out.println(Math.min(8.6, 9.7));// Note: Math.min() takes two arguments, not three
+        System.out.println(Math.max(9.6, 8.9));// Note: Math.max() takes two arguments, not three
+
+    }
+}
